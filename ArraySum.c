283c32ddb28ad255f8423941ca351262d55f1f25
Dipak9370 <112423122+Dipak9370@users.noreply.github.com>
2022-09-01 00:26:59 +0530

    Add files via upload

---
+++ ArraySum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int marks[5],n,i,sum=0;
+    printf("Enter number of element:");
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+    {
+        printf("Enter number %d:",i+1);
+        scanf("%d",&marks[i]);
+
+        sum += marks[i];
+    }
+    printf("Sum:%d",sum);
+    return 0;
+}
