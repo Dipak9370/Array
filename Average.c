79e0fc9b1f0845ca57d1d37802c7d4aacff13c2c
Dipak9370 <112423122+Dipak9370@users.noreply.github.com>
2022-09-01 00:36:29 +0530

    Update Average.c

--- Average.c
+++ Average.c
@@ -1,4 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int marks[5],n,i,sum=0,average;
+    printf("Enter number of element:");
+    scanf("%d",&n);
 
+    for(i=0;i<n;i++)
+    {
+        printf("Enter number %d:",i+1);
+        scanf("%d",&marks[i]);
 
-
-
+        sum += marks[i];
+    }
+  average = sum / n;
+    printf("Sum:%d",average);
+    return 0;
+}
